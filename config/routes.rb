f82aec9ffaf4d96f938b5fd2ebba5cb6f3202b7d
swong11 <sunwong11@gmail.com>
2015-06-05 07:35:30 +0000

    Build lessons

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Flixter::Application.routes.draw do
   devise_for :users
+  root 'static_pages#index'
+  resources :courses, :only => [:index, :show]
+  namespace :instructor do
+    resources :sections, :only => [] do 
+      resources :lessons, :only => [:new, :create]
+    end
+    resources :courses, :only => [:new, :create, :show] do 
+      resources :sections, :only => [:new, :create] 
+    end
+  end
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -55,11 +66,4 @@ Flixter::Application.routes.draw do
   #     resources :products
   #   end
 
-  root 'static_pages#index'
-  resources :courses, :only => [:index, :show]
-  namespace :instructor do
-    resources :courses, :only => [:new, :create, :show] do 
-      resources :sections, :only => [:new, :create]
-    end
-  end
-end
+ 
