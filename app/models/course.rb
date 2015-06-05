c4c94cd361426cc31162fd42b8fe25ce3ba1d594
swong11 <sunwong11@gmail.com>
2015-06-05 03:14:25 +0000

    Build sections

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,6 @@
 class Course < ActiveRecord::Base
 	belongs_to :user
+	has_many :sections
 
 	validates :title, :presence => { :message => "Can't be blank" }
 	validates :description, :presence => { :message => "Can't be blank" }
