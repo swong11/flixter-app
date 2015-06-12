611dbfdbb790dc278cadd945cdfaf9701a0a3939
swong11 <sunwong11@gmail.com>
2015-06-12 05:13:14 +0000

    Add drag and drop for sections

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Flixter::Application.routes.draw do
   resources :courses, :only => [:index, :show] do
     resources :enrollments, :only => :create 
   end
+  
   resources :lessons, :only => [:show]
+
   namespace :instructor do
     resources :lessons, :only => [:update] 
+    resources :sections, :only => [:update]
     resources :sections, :only => [] do 
       resources :lessons, :only => [:new, :create]
     end
