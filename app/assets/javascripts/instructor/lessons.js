6b2713258c21637ec4d9b6bb297918f6b84b53e6
swong11 <sunwong11@gmail.com>
2015-07-12 21:12:35 +0000

    CSS edits

---
+++ app/assets/javascripts/instructor/lessons.js
@@ -0,0 +1,30 @@
+$(function() {
+	$('.new-lesson-button').click(function(event) {
+    var lessonUrl = $( event.target).data('lesson-url');
+    $('#newLessonForm').attr('action', lessonUrl);
+  });
+
+  $('.lessons').sortable({
+    update: function( event, ui ) {
+      $.ajax({
+        type: 'PUT',
+        url: ui.item.data('update-url'),
+        dataType: 'json',
+        data: { lesson: { row_order_position: ui.item.index() } }
+      });
+    }
+  });
+
+ $('.sections').sortable({
+  update: function( event, ui ) {
+    $.ajax({
+      type: 'PUT',
+      url: ui.item.data('update-url'),
+      dataType: 'json',
+      data: { section: { row_order_position: ui.item.index() } }
+    });
+  }
+});
+
+});
+
